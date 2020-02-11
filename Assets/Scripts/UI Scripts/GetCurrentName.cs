6262df07f34a7b8ab47f83ac0492c047438534aa
angeliz_r <reyesangelize@gmail.com>
2020-02-11 21:39:39 +0800

    fixing commits n stuff

---
+++ Assets/Scripts/UI Scripts/GetCurrentName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GetCurrentName : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = GameManager.instance.GetName();
+    }
+
+}
